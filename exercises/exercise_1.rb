6bfef69ac0d0b1213cda7a305589b244db4db7a0
Myles <mylespatungan458@gmail.com>
2024-08-20 14:59:08 +0800

    Exercise 1: Create a BMI Calculator

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,10 +1,12 @@
+def cal_bmi(weight, height)
+  bmi = weight / height**2
+  print("\nYour BMI is #{bmi}\n")
+end
+
+
 print "BMI Calculation\n"
 
-print "Insert Weight\n"
-weight = gets.chomp.to_f
-print "\nInsert Height\n"
-height = gets.chomp.to_f
+print "Insert Weight & Insert Height\n"
+p cal_bmi(weight = gets.chomp.to_f , height = gets.chomp.to_f)
 
-bmi = weight / height**2
 
-print("\nYour BMI is #{bmi}\n")
