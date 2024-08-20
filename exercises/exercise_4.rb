748624ec1adc91c1e57efb0587bae89e8db076f3
Myles <mylespatungan458@gmail.com>
2024-08-20 14:44:20 +0800

    Exercise 2: Create Function to take an age to return a name

---
+++ exercises/exercise_4.rb
@@ -0,0 +1,13 @@
+def find_people(ages)
+  people = {john: 21, jenifer: 26, jake: 30}
+
+  people.each do |name, age|
+    if age == ages
+      return name
+    end
+  end
+end
+
+p find_people(30)
+
+
