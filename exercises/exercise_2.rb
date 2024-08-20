9ba8a1d80ba655542d8b79587f76190b1f4bae63
Myles <mylespatungan458@gmail.com>
2024-08-20 14:48:31 +0800

    Exercise 2: Create a Function that knows if your an adult

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -1,10 +1,11 @@
-print "BMI Calculation\n"
-
-print "Insert Age\n"
-age = gets.chomp.to_i
-
-if age >= 18
-  print"Adult\n"
-else
-  print"Not Adult\n"
+def adult(age)
+  if age >= 18
+    print"Adult\n"
+  else
+    print"Not Adult\n"
+  end
 end
+
+print "Are you an Adult?\n"
+print "Insert Age\n"
+p adult(age = gets.chomp.to_i)
