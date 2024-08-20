d6f00d08f7bbc389281d92f49ccf1685148c8f4e
Myles <mylespatungan458@gmail.com>
2024-08-20 14:26:01 +0800

    Exercise 2: Create Function to Get the adult ages in an Array

---
+++ exercises/exercise_3.rb
@@ -0,0 +1,13 @@
+ages = [20, 32, 24, 16, 12]
+
+def who_is_adult(ages)
+  adult = []
+  ages.each do |age|
+    if age >= 18
+      adult << age
+    end
+  end
+  return adult
+end
+
+who_is_adult(ages)
